586f261adf8c917a214ace182d49e6b40c3dfa45
Chaudhary-Muhammad-Irfan <chirfan2002muhammad@gmail.com>
2024-10-09 21:40:37 +0500

    Callback and callback hell in JS

--- app.js
+++ app.js
@@ -8,3 +8,39 @@ setTimeout(() => {
 }, 4000);
 console.log("three");
 console.log("four");
+
+
+// Callbacks in JS
+// A function that is passed as a parameter in an other function is called callback function
+function sum(a,b){
+    console.log(a+b);
+}
+function callBackFunction(a,b,sumFunction)
+{
+    sumFunction(a,b);
+}
+
+// Now calling the funciton
+callBackFunction(2,3,sum);
+
+
+// Callback hell .  also known as paramid of doom
+// a problem related to nested callbacks mean a callback function has another callback function
+
+function getData(dataId, callBackFunction) {
+    setTimeout(() => {
+        console.log("Data is : ", dataId);
+        if(callBackFunction)
+        {
+            callBackFunction();
+        }
+    }, 2000);
+}
+
+getData(1, ()=>{
+    getData(2,()=>{
+        getData(3,()=>{
+            getData(4);
+        })
+    })
+})
