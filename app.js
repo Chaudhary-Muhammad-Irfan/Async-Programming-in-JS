913c1527a8fa3b1e9098ef3e68051aece1baa70b
Chaudhary-Muhammad-Irfan <chirfan2002muhammad@gmail.com>
2024-10-10 21:25:42 +0500

    Promises in JS , states of promise . than and catch and their by default parameters

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ setTimeout(() => {
     // Set time out a special function that takes the time that after how much time i should execute the statements.
     // and the statements after that function are executed without waiting for the complition of this funciton . 
     // this is async programming
-    console.log("waiting for 4000 milli seconds mean 4 seconds");
+    
+    //console.log("waiting for 4000 milli seconds mean 4 seconds");
 }, 4000);
 console.log("three");
 console.log("four");
@@ -43,4 +44,33 @@ getData(1, ()=>{
             getData(4);
         })
     })
+})
+
+
+// Promises in JS
+
+// Promise is special type of objects in JS . A promise has 3 states . reject , pending and fulfill/resolve
+// Each promise has two parameters that are resolve and reject they themselves are function written by JS.
+let promise =new Promise((resolve , reject)=>{
+    console.log("I'm a promise");
+    resolve("Sucsess");
+    
+    
+    //reject("Some error");
+})
+
+// We don't write promises . We use API's and those API's return promises to us and we just handle those promises.
+
+// we use .than and .catch methods to handle promises
+
+let promise2=new Promise((resolve,reject)=>{
+    resolve("Success");
+})
+
+// catch and then have by default parameters that are res and err respectively.
+promise2.then((res)=>{
+    console.log("Promise is fulfilled", res);
+})
+promise.catch((err)=>{
+     console.log("Promise is not fulfilled",err);
 })
